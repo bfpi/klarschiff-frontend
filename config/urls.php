4215986f0a715b430835e3513bb8d54d94f9f830
Niels Bennke <bennke@bfpi.de>
2015-07-17 14:40:57 +0200

    Verbesserungen der Datenstruktur (keine Definitionsdopplungen)

--- config/urls.php
+++ config/urls.php
@@ -1,12 +1,14 @@
 <?php
 
-$base = "http://support.klarschiff-hro.de";
-define("BASE_URL", $base . "/");
+define("BASE_URL", "http://support.klarschiff-hro.de/");
 
 define("BACKEND_URL", BASE_URL . "backend/");
-define("FRONTEND_URL", BASE_URL . "");
+define("FRONTEND_URL", BASE_URL);
 define("MAP_URL", FRONTEND_URL . "map.php");
 define("MOBILE_FRONTEND_URL", BASE_URL . "mobil");
-define("MELDUNGEN_WFS_URL", BASE_URL . "geodienste/klarschiff/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=hro-demo.klarschiff.meldungen&outputFormat=GeoJSON");
-define("STADTTEILE_WFS_URL", BASE_URL . "geodienste/klarschiff/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=hro-demo.klarschiff.ortsteile&outputFormat=GeoJSON");
-define("GEORSS_URL", BASE_URL . "geodienste/klarschiff/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=hro-demo.klarschiff.meldungen_beobachtungsflaechen&outputFormat=GeoRSS&srsName=EPSG:4326");
+
+$wfs_query = BASE_URL . "geodienste/klarschiff/wfs?service=WFS&version=1.0.0&request=GetFeature";
+
+define("MELDUNGEN_WFS_URL", $wfs_query . "&typeName=hro-support.klarschiff.meldungen&outputFormat=GeoJSON");
+define("STADTTEILE_WFS_URL", $wfs_query . "&typeName=hro-support.klarschiff.ortsteile&outputFormat=GeoJSON");
+define("GEORSS_URL", $geoserver_base . "&typeName=hro-support.klarschiff.meldungen_beobachtungsflaechen&outputFormat=GeoRSS&srsName=EPSG:4326");
