bbea327751859e383f3b2207f9814f41ff63116d
Alexander <kruth@bfpi.de>
2015-09-16 15:32:55 +0200

    Konfiguration für Mapserver

--- config/urls.php
+++ config/urls.php
@@ -7,9 +7,8 @@ define("FRONTEND_URL", BASE_URL . "pc/");
 define("MAP_URL", FRONTEND_URL . "map.php");
 define("MOBILE_FRONTEND_URL", BASE_URL . "mobil");
 
-$geoserver_base = BASE_URL . "ows/klarschiff/";
-$wfs_query = $geoserver_base . "ows?service=WFS&version=1.0.0&request=GetFeature&outputFormat=application/json";
+$wfs_query = BASE_URL . "geodienste/klarschiff?service=WFS&version=1.0.0&request=GetFeature";
 
-define("MELDUNGEN_WFS_URL", $wfs_query . "&typeName=klarschiff_hgw:vorgaenge");
-define("ORTSTEILE_WFS_URL", $wfs_query . "&typeName=klarschiff_hgw:stadtteile");
-define("GEORSS_URL", $geoserver_base . "wms/reflect?layers=klarschiff_hgw:klarschiff_wfs_georss&format=rss");
+define("MELDUNGEN_WFS_URL", $wfs_query . "&typeName=klarschiff.meldungen&outputFormat=GeoJSON");
+define("ORTSTEILE_WFS_URL", $wfs_query . "&typeName=klarschiff.stadtteile&outputFormat=GeoJSON");
+define("GEORSS_URL", $wfs_query . "&typeName=klarschiff.georss&outputFormat=GeoRSS&srsName=EPSG:4326");
