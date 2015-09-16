9c6c718686404a4ef90fe443e24374f5ae419625
Alexander <kruth@bfpi.de>
2015-09-16 15:32:55 +0200

    Konfiguration für Mapserver
    Conflicts:
    	config/urls.php

--- config/urls.php
+++ config/urls.php
@@ -7,8 +7,8 @@ define("FRONTEND_URL", BASE_URL);
 define("MAP_URL", FRONTEND_URL . "map.php");
 define("MOBILE_FRONTEND_URL", BASE_URL . "mobil");
 
-$wfs_query = BASE_URL . "geodienste/klarschiff/wfs?service=WFS&version=1.0.0&request=GetFeature";
+$wfs_query = BASE_URL . "geodienste/klarschiff?service=WFS&version=1.0.0&request=GetFeature";
 
-define("MELDUNGEN_WFS_URL", $wfs_query . "&typeName=hro-support.klarschiff.meldungen&outputFormat=GeoJSON");
-define("ORTSTEILE_WFS_URL", $wfs_query . "&typeName=hro-support.klarschiff.ortsteile&outputFormat=GeoJSON");
-define("GEORSS_URL", $wfs_query . "&typeName=hro-support.klarschiff.meldungen_beobachtungsflaechen&outputFormat=GeoRSS&srsName=EPSG:4326");
+define("MELDUNGEN_WFS_URL", $wfs_query . "&typeName=klarschiff.meldungen&outputFormat=GeoJSON");
+define("ORTSTEILE_WFS_URL", $wfs_query . "&typeName=klarschiff.stadtteile&outputFormat=GeoJSON");
+define("GEORSS_URL", $wfs_query . "&typeName=klarschiff.georss&outputFormat=GeoRSS&srsName=EPSG:4326");
