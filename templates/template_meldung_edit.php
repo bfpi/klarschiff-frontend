1daf874cd80b5e017f6d1266c090bc68586a40d8
ro90 <oest@bfpi.de>
2017-10-19 07:02:10 +0200

    Hinweis für Foto-Upload verschoben und Style verändert

--- templates/template_meldung_edit.php
+++ templates/template_meldung_edit.php
@@ -20,12 +20,11 @@
   <h4 style="margin-bottom:8px">weitere Angaben</h4>	
   <label for="beschreibung" style="margin-bottom:3px">Beschreibung</label>
   <textarea name="beschreibung">${beschreibung}</textarea>
-  <p style="font-size:0.8em;margin-bottom:8px;"><b>
+  <label for="foto" style="margin-bottom:3px">Foto <span style="font-style:italic;color:#d81920">(Dateigröße maximal 4 MB!)</span></label>
+  <p class="help">
     Bitte laden Sie ein Foto des beschriebenen Sachverhaltes hoch. Es ist hilfreich, wenn das Foto auch die nähere Umgebung zeigt.
     Dies erleichtert die Auffindbarkeit und ermöglicht den Fachbereich eine erste Einschätzung vorzunehmen.
-  </b></p>
-
-  <label for="foto" style="margin-bottom:3px">Foto <span style="font-style:italic;color:#d81920">(Dateigröße maximal 4 MB!)</span></label>
+  </p>
   <input type="file" name="foto"/>
   <table id="fotos"></table>
 
